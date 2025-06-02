45c890002a3e998f92034a6c1e65b88bcdce7ab5
vrush119 <vrushali.kudanekar@gmail.com>
2025-06-02 14:57:32 +0530

    second commit 3 files

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+<<<<<<< HEAD
    plugins: [react()],
   base: "/react-final-project-coursera/",
  
+=======
+  base: "/react-final-project-coursera/",
+  plugins: [react()],
+>>>>>>> 2c600e0 (Setup for GitHub Pages deployment)
 })
